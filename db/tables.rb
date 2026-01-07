cfdb57365e845b736e3962b57c8b492292e3062d
kuroibara10 <kazukashika100@gmail.com>
2026-01-07 12:39:15 +0200

    Create all tables

--- db/tables.rb
+++ db/tables.rb
@@ -10,3 +10,58 @@ DB.execute <<-SQL
     password TEXT
   );
 SQL
+
+#Table projects
+DB.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS projects(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    description TEXT,
+    email_origin TEXT,
+    user_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+  );
+SQL
+#Table members
+DB.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS members(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT,
+    is_admin INTEGER DEFAULT 0,
+    project_id INTEGER,
+    FOREIGN KEY(project_id) REFERENCES projects(id)
+  );
+SQL
+
+#Table discussions
+DB.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS discussions(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT,
+    message TEXT,
+    project_id INTEGER,
+    FOREIGN KEY(project_id) REFERENCES projects(id)
+  );
+SQL
+
+#Table tasks
+DB.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS tasks(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task TEXT,
+    email TEXT,
+    project_id INTEGER,
+    FOREIGN KEY(project_id) REFERENCES projects(id)
+  );
+SQL
+
+#Table attachements
+DB.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS attachements(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    url_place TEXT,
+    email TEXT,
+    project_id INTEGER,
+    FOREIGN KEY(project_id) REFERENCES projects(id)
+  );
+SQL
